f9b017463c9574fae7770d9d66d0fd214a4da131
kredenac <zacementirano@gmail.com>
2018-10-28 14:26:22 +0100

    Refactoring py code, added filtering, tested decision trees

--- source/makePLA.py
+++ source/makePLA.py
@@ -1,11 +1,10 @@
 # preprocessing c table into something more managable
 import pyeda
 
-
 # only white figures movement are of concern
 # isWhiteTurn, isRookCaptured, BKx, BKy, WKx, WKy, WRx, WRy
 lookup = {}
-BOARD_SIZE = 3
+BOARD_SIZE = 4
 
 # number of bits for coding the direction
 UDLR_BITS = 4
@@ -35,27 +34,48 @@ WKy = 5
 WRx = 6
 WRy = 7
 
-print ("reading file...")
-INPUT_TABLE_FILE = f'chessDict{BOARD_SIZE}x{BOARD_SIZE}.txt' 
-with open(INPUT_TABLE_FILE, 'r') as f:
-    next(f) # skips file comment
-    # make a dict
-    for (i, line) in enumerate(f):
-        isWhiteTurn, isRookCaptured, tBKx, tBKy, tWKx, tWKy, \
-            tWRx, tWRy = [int(i) for i in line.split()]
-        
-        tup = (isWhiteTurn, isRookCaptured, tBKx, tBKy, tWKx, tWKy, tWRx, tWRy)
-        if (i % 2 == 0):
-            key = tup
-        else:
-            lookup[key] = tup
+def main():
+    readInputFile()
+    convertToBits()
+
+    BEFORE_MINIMIZATION_LENGTH = len(bitLookup)
+    print('before minimization lenght = ', BEFORE_MINIMIZATION_LENGTH)
+    someKey = next(iter(bitLookup))
+    print('Key length = ', len(someKey), 'Value length = ', len(bitLookup[someKey]) )
+
+    GENERATED_PLA = f"original{BOARD_SIZE}x{BOARD_SIZE}.pla"
+    print("writing table to pla file...")
+    tableToPla(bitLookup, GENERATED_PLA)
+
+    filteredLookup =  filterTable(bitLookup, r"1..1........")
+    printTable(filteredLookup)
+
+    #MINIMIZED_OUTPUT_FILE = f"minimized{BOARD_SIZE}x{BOARD_SIZE}.pla"
+    #print(f"Saved to {MINIMIZED_OUTPUT_FILE}")
+
+    # writeCsv(bitLookup)
+    print("Finished.")
+
+    
+def readInputFile():
+    print ("reading file...")
+    INPUT_TABLE_FILE = f'tableGen/chessDict{BOARD_SIZE}x{BOARD_SIZE}.txt' 
+    with open(INPUT_TABLE_FILE, 'r') as f:
+        next(f) # skips file comment
+        # make a dict
+        for (i, line) in enumerate(f):
+            isWhiteTurn, isRookCaptured, tBKx, tBKy, tWKx, tWKy, \
+                tWRx, tWRy = [int(i) for i in line.split()]
+            
+            tup = (isWhiteTurn, isRookCaptured, tBKx, tBKy, tWKx, tWKy, tWRx, tWRy)
+            if (i % 2 == 0):
+                key = tup
+            else:
+                lookup[key] = tup
+    print('finished!')
+    print("Items read = ", len(lookup))
 
 
-#for key, val in lookup.items():
-#   print (key, val)
-
-print('finished!')
-print("Items read = ", len(lookup))
 
 # N E W code
 # maps from current game state into optimal move
@@ -64,10 +84,6 @@ print("Items read = ", len(lookup))
 # value: up, down left, right each 1 bit for king
 #   up, down, left, right each 1 bit for rook + num bits x2 for how many fields (first x then y axis)
 # kup, kdwn, kleft, kright, rup, rdwn, rlft, rrght, x3, x2, x1, y3, y2, y1
-
-# O L D
-# key: 3 bits for each BKx, BKy, WKx, Wky, Wrx, Wry = 18 bits
-# value: 1 bit king/rook, up, down, left, right, 2 bits - how many times 
 bitLookup = {}
 
 # returns a list of bools indicating the number
@@ -106,55 +122,56 @@ def getRookDirectionAndDistance(key, value):
 def boolList2BinString(lst):
     return ''.join(['1' if x else '0' for x in lst])
 
-print("Converting to bits...")
-for key, val in lookup.items():
-    keyBits = numberIntoBits(key[BKx])
-    keyBits += numberIntoBits(key[BKy]) 
-    keyBits += numberIntoBits(key[WKx]) 
-    tmp = numberIntoBits(key[WKy])
-    keyBits += tmp 
-    keyBits += numberIntoBits(key[WRx]) 
-    keyBits += numberIntoBits(key[WRy]) 
-    
-    valueBits = ''
-    isKing = didKingPlay(key, val)
-
-    if isKing:
-        valueBits += boolList2BinString( getKingDirection(key, val) )
-        # fill with 0s on the right for rook bits
-        valueBits = valueBits.ljust(TOTAL_VAL_LEN, '0')
-        #print("king valueBits len", len(valueBits))
-    else:
-        up,down,left,right,distance = getRookDirectionAndDistance(key, val)
-        valueBits += boolList2BinString([up,down,left,right])
-        distanceBits = numberIntoBits(distance)
-        if up or down:
-            #print(f"valueBits = {valueBits}")
-            #print(f"distanceBits old = {distanceBits}")
-            distanceBits = distanceBits.ljust(POS_BIT_LEN * 2, '0')
-            #print(f"distanceBits new = {distanceBits}")
-        else:
-            distanceBits = distanceBits.zfill(POS_BIT_LEN * 2)
+def convertToBits():
+    print("Converting to bits...")
+    for key, val in lookup.items():
+        keyBits = numberIntoBits(key[BKx])
+        keyBits += numberIntoBits(key[BKy]) 
+        keyBits += numberIntoBits(key[WKx]) 
+        tmp = numberIntoBits(key[WKy])
+        keyBits += tmp 
+        keyBits += numberIntoBits(key[WRx]) 
+        keyBits += numberIntoBits(key[WRy]) 
         
-        valueBits += distanceBits
-        # fill with 0s on the left for king bits
-        valueBits = valueBits.zfill(TOTAL_VAL_LEN)
-        if up or down:
-            print("UP or DOWN : ", valueBits)
-        #print("rook valueBits len", len(valueBits))
+        valueBits = ''
+        isKing = didKingPlay(key, val)
 
-    bitLookup[keyBits] = valueBits
+        if isKing:
+            valueBits += boolList2BinString( getKingDirection(key, val) )
+            # fill with 0s on the right for rook bits
+            valueBits = valueBits.ljust(TOTAL_VAL_LEN, '0')
+            #print("king valueBits len", len(valueBits))
+        else:
+            up,down,left,right,distance = getRookDirectionAndDistance(key, val)
+            valueBits += boolList2BinString([up,down,left,right])
+            distanceBits = numberIntoBits(distance)
+            if up or down:
+                #print(f"valueBits = {valueBits}")
+                #print(f"distanceBits old = {distanceBits}")
+                distanceBits = distanceBits.ljust(POS_BIT_LEN * 2, '0')
+                #print(f"distanceBits new = {distanceBits}")
+            else:
+                distanceBits = distanceBits.zfill(POS_BIT_LEN * 2)
+            
+            valueBits += distanceBits
+            # fill with 0s on the left for king bits
+            valueBits = valueBits.zfill(TOTAL_VAL_LEN)
+            #print("rook valueBits len", len(valueBits))
+
+        bitLookup[keyBits] = valueBits
 
 
-def printTable():
-    for key, val in bitLookup.items():
+def printTable(table):
+    print("printing table.")
+    for key, val in table.items():
         print(key, val)
-#printTable()
-#exit()
-BEFORE_MINIMIZATION_LENGTH = len(bitLookup)
-print('bitLookup lenght = ', BEFORE_MINIMIZATION_LENGTH)
-someKey = next(iter(bitLookup))
-print('Key length = ', len(someKey), 'Value length = ', len(bitLookup[someKey]) )
+    print(f"Table has {len(table)} items.")
+
+
+
+def filterTable(table, filterString):
+    import re
+    return  {k:v for k,v in table.items() if re.match(filterString, v)}
 
 def tableToPla(table, fileName):
     f = open(fileName, "w")
@@ -171,15 +188,10 @@ def tableToPla(table, fileName):
         print(f"{key} {val}", file=f)
     f.close()
 
-GENERATED_PLA = f"original{BOARD_SIZE}x{BOARD_SIZE}.pla"
-print("writing table to pla file...")
-tableToPla(bitLookup, GENERATED_PLA)
 
-MINIMIZED_OUTPUT_FILE = f"minimized{BOARD_SIZE}x{BOARD_SIZE}.pla"
-#print(f"Saved to {MINIMIZED_OUTPUT_FILE}")
 
 def writeCsv(table):
-    print("writin csv")
+    print("writing csv")
     import csv
     fieldnames = ['BKX1', 'BKX2', 'BKY1', 'BKY2', 'WKX1', 'WKX2', 'WKY1', 'WKY2', 'WRX1', 'WRX2', 'WRY1', 'WRY2']
     fieldnames.extend(['KU', 'KD', 'KL', 'KR', 'RU', 'RD', 'RL', 'RR', 'RX1', 'RX2', 'RY1', 'RY2'])
@@ -200,12 +212,12 @@ def writeCsv(table):
             writer.writerow(row)
     print("finished writing csv")
 
-writeCsv(bitLookup)
 
-def analyzeOutput():
+
+def analyzeOutput(outputFile):
     numLines = 0
     countDontCare = 0
-    with open(MINIMIZED_OUTPUT_FILE, 'r') as f:
+    with open(outputFile, 'r') as f:
         for i, l in enumerate(f):
             countDontCare += l.count("-")
             numLines += 1
@@ -217,4 +229,8 @@ def analyzeOutput():
     percentDontCare = countDontCare/ (numLines * TOTAL_KEY_LEN)
     print(f"That's {percentDontCare}% dont care bits.")
 
-print("Finished.")
+
+
+
+if __name__ == "__main__":
+    main()
